e0aa6b0217b6fb399bc57b8eb29f3befa9f3c864
Sneha_KS <88614694+SnehaKShivaraj@users.noreply.github.com>
2022-08-21 16:01:44 +0000

    Sync LeetCode submission - Two Sum (csharp)

--- my-folder/problems/two_sum/solution.cs
+++ my-folder/problems/two_sum/solution.cs
@@ -1,20 +1,42 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        var indexes = new int[2];
-        for(int i=0;i<nums.Length;i++)
-        {
-            for(int j=0;j<nums.Length-1;j++)
-            {
-                if(i==j)
-                    continue;
-                if(nums[i]+nums[j] == target)
+        
+        var result = new int[2];
+        return TwoSumRecursive(nums, target, 0, result);
+        
+    }
+    private int GetIndex(int[] nums, int rem, int k)
+    {
+        int index =0;
+         for(int j=k;j< nums.Length;j++)
+         {
+                if(nums[j] == rem)
                 {
-                    indexes[0]=i;
-                    indexes[1]=j;
-                    return indexes;
-                } 
-            }        
-        }
-    return indexes;
+                    index =j;
+                    break;
+                }
+                    
+         }
+        return index;
+        
+    }
+    private int[] TwoSumRecursive(int[] nums, int target, int i, int[] resultArray) {
+        
+            if (i >= nums.Length)
+                return new int[0];
+
+            int rem = target - nums[i];
+            if (nums.Contains(rem))
+            {
+                if (rem != nums[i] || (rem == nums[i] && nums.Count(s => s == rem) > 1))
+                {
+                    resultArray[0] = i;
+                    resultArray[1] = GetIndex(nums, rem, i + 1);
+                    return resultArray;
+                }
+
+            }
+             return  TwoSumRecursive(nums, target, i+1, resultArray);
+        
     }
 }
