099eef6ddadc7de1e31627d96344dbbe2cc3fbbd
Sneha_KS <88614694+SnehaKShivaraj@users.noreply.github.com>
2023-10-10 14:33:58 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (csharp)

--- my-folder/problems/longest_substring_without_repeating_characters/solution.cs
+++ my-folder/problems/longest_substring_without_repeating_characters/solution.cs
@@ -1,19 +1,30 @@
 public class Solution {
     public int LengthOfLongestSubstring(string s) {
-              int l=0;
-      var set = new List<char>();
-      int max=0;
-      for(int r=0;r<s.Length;r++)
-      {
-         while(set.Contains(s[r]))
-         {
-             set.RemoveAt(0);
-         }
-          set.Add(s[r]);
-          max = max>set.Count?max:set.Count;
-             
-      }
-      return max;
+        
+        var set = new List<char>();
+        int i = 0, j = 0, max = 0;
+        while(j < s.Length)
+        {
+            if(!set.Contains(s[j]))
+            {
+                set.Add(s[j]);
+            }
+            else{
+                while(s[j] != s[i])
+                {
+                    set.Remove(s[i]);
+                    i++;
+                }
+                i++;
+            } 
+            max = Maximum(max, j - i + 1);
+            j++;   
+        }
+        return max;
+
     }
-    
+    private static int Maximum(int num1, int num2)
+    {
+        return num1 > num2 ? num1 : num2;
+    }
 }
