66034e051679218a010b2c7e5e11ff7fcede9287
Sneha_KS <88614694+SnehaKShivaraj@users.noreply.github.com>
2023-11-17 08:05:26 +0000

    Sync LeetCode submission - Partition Equal Subset Sum (csharp)

--- my-folder/problems/partition_equal_subset_sum/solution.cs
+++ my-folder/problems/partition_equal_subset_sum/solution.cs
@@ -1,38 +1,40 @@
 public class Solution {
     public bool CanPartition(int[] nums) {
-    int sum = 0;
-    foreach(int num in nums){
-        sum += num;
-    }  
-    
-    if(sum%2 == 1) return false;
+        /*
+        [1,5,11,5] sum = 23/2 =11 
+        11 - 1 = 10 - 5 = 5 - 5 = 0
+        11 = 11
 
-    int l = nums.Length;
-    sum /= 2;
-    var dp = new bool[l+1, sum+1];
-    for(int i = 0; i<=l; i++){
-        for(int j = 0; j<=sum; j++){
-                
-            if(j == 0 ){
-                dp[i, j] = true;    
-            }                
-            else{
-                    dp[i, j] = false;
-            }
+        */ 
+        int sum = 0;
+
+        foreach(int i in nums){
+            sum+= i;
         }
-    }
+
+        if(sum % 2 == 1){
+            return false;
+        } 
         
-    for(int i = 1; i <= l; i++){
-        for(int j = 1; j <= sum; j++){
-            if(nums[i-1] > j){
-                 dp[i, j] = dp[i-1, j];
-            }
-            else{
-                 dp[i, j] = dp[i-1, j - nums[i-1]] || dp[i-1, j];
-            }
-        }
+        return SubSetSum(nums, sum/2, nums.Length, new Dictionary<(int, int), bool>()); 
     }
-    return dp[nums.Length, sum];
-    
+    static bool SubSetSum(int[] nums, int sum, int n, IDictionary<(int, int), bool> dict){
+
+        if(sum == 0 ){
+            return true;
+        }
+        
+        if(n == 0){
+            return false;
+        }
+        (int, int) key = (sum, n);
+
+        if(dict.ContainsKey(key))
+            return dict[key];
+
+        if(sum >= nums[n - 1]){
+            return dict[key] = SubSetSum(nums, sum - nums[n - 1], n - 1, dict) || SubSetSum(nums, sum, n - 1, dict) ;
+        }
+        return dict[key] = SubSetSum(nums, sum, n - 1, dict);
     }
 }
