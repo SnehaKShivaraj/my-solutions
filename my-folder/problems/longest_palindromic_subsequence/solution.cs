c66215cb010e9fbe2bc9c2ff5a772059c994b5bd
Sneha_KS <88614694+SnehaKShivaraj@users.noreply.github.com>
2023-11-28 08:46:02 +0000

    Sync LeetCode submission - Longest Palindromic Subsequence (csharp)

---
+++ my-folder/problems/longest_palindromic_subsequence/solution.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int LongestPalindromeSubseq(string s) {
+        string reversedString = "";
+
+        for(int i = s.Length - 1 ; i >= 0; i--){
+            reversedString += s[i];
+        }
+        return getMaxLengthOfSubseq(s, reversedString, s.Length - 1, s.Length - 1, new Dictionary<(int, int), int>());
+    }
+     private static int getMaxLengthOfSubseq(string text1, string text2, int i, int j, IDictionary<(int, int), int> memo){
+        if( i < 0 || j < 0){
+            return 0;
+        }
+
+        var key = (i, j);
+        if(memo.ContainsKey(key)){
+            return memo[key];
+        }
+
+        if(text1[i] == text2[j]){
+            return memo[key] = 1 + getMaxLengthOfSubseq(text1, text2, i - 1, j - 1, memo);
+        }
+
+        return memo[key] =  Math.Max(getMaxLengthOfSubseq(text1, text2, i - 1, j, memo), 
+                        getMaxLengthOfSubseq(text1, text2, i, j - 1, memo));
+    }
+}
