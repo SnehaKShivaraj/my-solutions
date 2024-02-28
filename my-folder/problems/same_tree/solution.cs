89152ecd96f00bd131a5ee220b997bdbc9db6576
Sneha_KS <88614694+SnehaKShivaraj@users.noreply.github.com>
2024-02-28 04:44:44 +0000

    Sync LeetCode submission - Same Tree (csharp)

--- my-folder/problems/same_tree/solution.cs
+++ my-folder/problems/same_tree/solution.cs
@@ -13,13 +13,9 @@
  */
 public class Solution {
     public bool IsSameTree(TreeNode p, TreeNode q) {
-       return  AreSame(p, q);
-    }
-    private static bool AreSame(TreeNode p, TreeNode q){
         if(p == null || q == null){
             return p == q;
         }
-
-        return p.val == q.val && AreSame(p.left, q.left) && AreSame(p.right, q.right);
+        return p.val == q.val && IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
     }
 }
